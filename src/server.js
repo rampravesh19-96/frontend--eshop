64579e55aeb86bc95cde75414a63bcc6ecfd106c
ram pravesh singh <ramps1996@hotmail.com>
2024-05-08 18:00:50 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,7 @@
+
+// export const server = "http://localhost:8000/api/v2";
+export const server = "https://backend-eshop-five.vercel.app/api/v2";
+// export const socketServer = "http://localhost:4000/";
+export const socketServer = "https://socket-khaki.vercel.app/";
+
+
